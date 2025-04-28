032da2419b154c2f13cd4acce718cf0d31027845
dr-x <dr-x@mail.com>
2025-04-28 11:09:03 +0800

    feat: gitignore

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -12,6 +12,12 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks(id) {
+                    if (id.includes("antd")) {
+                        return "ui"
+                    }
+                    if (id.includes("ant-design")) {
+                        return "ui-icon"
+                    }
                     if (id.includes("node_modules")) {
                         return "vendor"
                     }
